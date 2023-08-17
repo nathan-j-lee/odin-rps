383248fa1d3faac868d9650efab35d4a6f9f28e9
nathan-j-lee <127238376+nathan-j-lee@users.noreply.github.com>
2023-08-16 19:55:15 -0700

    Add win and lose logic and result div

--- rps.js
+++ rps.js
@@ -30,29 +30,67 @@ scissors.addEventListener('click', function () {
     playRound("scissors");
 });
 
-const div = document.querySelector('#result');
+userScore = 0;
+compScore = 0;
+
+const userScoreBox = document.querySelector('#userScore');
+const compScoreBox = document.querySelector('#compScore');
+const div = document.querySelector("#result");
+
+userScoreBox.textContent = "Your score: " + userScore;
+compScoreBox.textContent = "Enemy score: " + compScore;
 
 function playRound(choice) {
     computer = randomChoice();
     console.log("You picked " + choice + ".");
     console.log("Computer picked " + computer + ".");
     res = "error if you see this";
+    
     if (choice == "rock") {
         if (computer == "rock") res = "It's a tie!";
-        else if (computer == "paper") res = "You lose!";
-        else if (computer == "scissors") res = "You win!";
+        else if (computer == "paper") {
+            res = "You lose!";
+            compScore++;
+        }
+        else if (computer == "scissors") {
+            res = "You win!";
+            userScore++;
+        }
     }
     else if (choice == "paper") {
-        if (computer == "rock") res = "You win!";
+        if (computer == "rock") {
+            res = "You win!";
+            userScore++;
+        }
         else if (computer == "paper") res = "It's a tie!";
-        else if (computer == "scissors") res = "You lose!";
+        else if (computer == "scissors") {
+            res = "You lose!";
+            compScore++;
+        }
     }
-
     else if (choice == "scissors") {
-        if (computer == "rock") res = "You lose!";
-        else if (computer == "paper") res = "You win!";
+        if (computer == "rock") {
+            res = "You lose!";
+            compScore++;
+        }
+        else if (computer == "paper") {
+            res = "You win!";
+            userScore++;
+        }
         else if (computer == "scissors") res = "It's a tie!";
     }
-
-    div.textContent=res;
+    if (userScore >= 5) {
+        res = "You got to 5 points! You Win! Restarting...";
+        userScore=0;
+        compScore=0;
+        
+    }
+    if (compScore >= 5) {
+        res = "Computer got to 5 points! You Lose! Restarting...";
+        userScore=0;
+        compScore=0;
+    }
+    userScoreBox.textContent = "Your score: " + userScore;
+    compScoreBox.textContent = "Enemy score: " + compScore;
+    div.textContent = res;
 }
